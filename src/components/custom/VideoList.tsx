1337716a4554863df09f1e2fbc57cc54741092d6
monster-1951 <shivashankarsince19@gmail.com>
2025-02-12 00:15:43 +0530

    videos 3

--- src/components/custom/VideoList.tsx
+++ src/components/custom/VideoList.tsx
@@ -1,27 +1,26 @@
-"use client";
-import Link from "next/link";
-import { videos } from "@/constants/videos";
-import VideoCard from "./VideoCard";
-import { useEffect, useState } from "react";
+'use client';
+import Link from 'next/link';
+import { videos } from '@/constants/videos';
+import VideoCard from './VideoCard';
+import { useEffect, useState } from 'react';
 
 const VideoList = () => {
-  const [Mode, setMode] = useState<string | null>("");
-  const [suggestedVideos, setSuggestedVideos] = useState<number[]>([]);
+  const [Mode, setMode] = useState<string | null>('');
+  const [suggestedVideos, setSuggestedVideos] = useState<string[]>([]); // Ensure ids are stored as strings
 
   useEffect(() => {
-    const storedMode =
-      typeof window !== "undefined" ? localStorage.getItem("Mode") : null;
+    const storedMode = typeof window !== 'undefined' ? localStorage.getItem('Mode') : null;
     setMode(storedMode);
 
-    if (typeof window !== "undefined") {
-      const storedSuggestedVideos = localStorage.getItem("suggestedVideos");
+    if (typeof window !== 'undefined') {
+      const storedSuggestedVideos = localStorage.getItem('suggestedVideos');
       if (storedSuggestedVideos) {
         setSuggestedVideos(JSON.parse(storedSuggestedVideos));
       }
     }
   }, []);
 
-  if (Mode === "Parent Mode") {
+  if (Mode === 'Parent Mode') {
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 mb-28">
         {videos?.map((video) => (
@@ -32,11 +31,12 @@ const VideoList = () => {
       </div>
     );
   } else {
+    console.log(suggestedVideos);
     return (
       <>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 mb-28">
           {videos?.map((video) => {
-            if (suggestedVideos.includes(parseInt(video.id))) {
+            if (suggestedVideos.includes(video.id)) {
               return (
                 <Link key={video.id} href={`/Videos/${video.id}`}>
                   <VideoCard video={video} />
@@ -45,9 +45,9 @@ const VideoList = () => {
             }
           })}
         </div>
-        {suggestedVideos.length == 0 && (
+        {suggestedVideos.length === 0 && (
           <div className="text-red-500 font-bold mx-auto w-fit">
-            No Videos suggested by your parent . Ask them to suggest
+            No videos suggested by your parent. Ask them to suggest.
           </div>
         )}
       </>
